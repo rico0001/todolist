fef4302f0d4c14775b395772f29e11fbff18b5fc
eric <ericnjuguna047@gmail.com>
2024-06-20 23:42:41 +0300

    box-shadow

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-window.onload = function() {
-    console.log("Js Connected");
+window.onload = function () {
+  console.log("Js Connected");
 };
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-function sendRequest(){
+function sendRequest() {
+  const name = document.getElementById("name"); // get input element
 
-    const name = document.getElementById("name"); // get input element
+  const nameVal = name?.value; // get input element value
 
-    const nameVal = name?.value; // get input element value
-   
-    const phone =document.getElementById('Phone-Number');
+  const phone = document.getElementById("Phone-Number");
 
-    const phoneVal = phone?.value;
+  const phoneVal = phone?.value;
 
-    const date = document.getElementById("date");
+  const date = document.getElementById("date");
 
-    const dateVal = date?.value;
+  const dateVal = date?.value;
 
-    const time = document.getElementById("time");
+  const time = document.getElementById("time");
 
-    const timeVal = time?.value;
+  const timeVal = time?.value;
 
-    const Description = document.getElementById("Description");
+  const Description = document.getElementById("Description");
 
-    const DescriptionVal = Description?.value;
+  const DescriptionVal = Description?.value;
 
-    const myCheck = document.getElementById("myCheck");
+  const myCheck = document.getElementById("myCheck");
 
-    const myCheckVal = myCheck?.value;
+  const myCheckVal = myCheck?.checked;
 
-   // console.log(phoneVal)
+  if (nameVal === "") {
+    window.alert("Please Fill in all the fields");
+  }
 
-    const object = {
-         "name": nameVal,
-        "phone": phoneVal,
-        "date" : dateVal,
-        "time" : timeVal,
-        "Description" : DescriptionVal,
-        "myCheck"  : myCheckVal,
+  // console.log(phoneVal)
+  // research on how if statements work
+  //loop through your object to check for empty values
+  //if any value is empty alert user to fill in all the fields
+  //if all values return a vali value alert the user form submission was a sucesss
 
+  const object = {
+    name: nameVal,
+    phone: phoneVal,
+    date: dateVal,
+    time: timeVal,
+    Description: DescriptionVal,
+    myCheck: myCheckVal,
+  };
 
-     };
+  console.log(object);
 
-     console.log(object);
+  //localStorage.setItem(object);
 
-    window.alert("Form Submission Successful");
+  // window.alert("Form Submission Successful");
+}
+
+function buildloginLink() {
+  window.location.href = "login.html";
 }
